f2c34bbc496cd820f6a885951315185e6c4e3a98
Hari Reshmi <harireshmi22@gmail.com>
2024-11-25 22:15:36 +0530

    new changes

--- index.js
+++ index.js
@@ -1,33 +1,3 @@
-import Swiper from "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs";
-
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 2,
-  spaceBetween: 10,
-  // centeredSlides: true,
-  loop: true,
-  grabCursor: true,
-  freeMode: true,
-
-  autoplay: {
-    delay: 1500,
-    disableOnInteraction: false,
-  },
-
-  keyboard: {
-    enabled: true,
-  },
-
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
-
 // Register ScrollTrigger plugin
 gsap.registerPlugin(ScrollTrigger);
 
